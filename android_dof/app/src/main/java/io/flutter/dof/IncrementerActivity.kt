8d8943b2e74228a03d21347642e99b839ca4e38f
Matt Sullivan <matt.j.sullivan@gmail.com>
2018-08-10 18:21:03 -0700

    Added unused custom FlutterView code

--- android_dof/app/src/main/java/io/flutter/dof/IncrementerActivity.kt
+++ android_dof/app/src/main/java/io/flutter/dof/IncrementerActivity.kt
@@ -6,8 +6,24 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 
 class IncrementerActivity : FlutterActivity() {
 
+    private val _tag = "IncrementerActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
     }
+
+    /*
+    override fun createFlutterView(context: Context) : FlutterView {
+        val flutterEngine = FlutterNativeView(this)
+        val flutterView = FlutterView(this, null, flutterEngine)
+        flutterView.setInitialRoute("route1")
+        flutterView.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        Log.w(_tag, "Creating Flutter view")
+        setContentView(flutterView)
+        return flutterView
+    }*/
 }
