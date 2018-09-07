d88c1ed2c6ff0adde57311399cc36a9e437c7a97
Matt Sullivan <matt.j.sullivan@gmail.com>
2018-09-06 17:19:16 -0700

    Made the grid look nice

--- android_dof/app/src/main/java/io/flutter/dof/MainActivity.kt
+++ android_dof/app/src/main/java/io/flutter/dof/MainActivity.kt
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.widget.Toast
 
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.ArrayList
 
@@ -20,48 +19,43 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.ItemListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        // setSupportActionBar(toolbar)
 
         arrayList = ArrayList()
-        arrayList.add(DataModel("Item 1", R.drawable.ic_train_black_24dp, "#09A9FF"))
-        arrayList.add(DataModel("Item 2", R.drawable.ic_train_black_24dp, "#3E51B1"))
-        arrayList.add(DataModel("Item 3", R.drawable.ic_train_black_24dp, "#673BB7"))
-        arrayList.add(DataModel("Item 4", R.drawable.ic_train_black_24dp, "#4BAA50"))
-        arrayList.add(DataModel("Item 5", R.drawable.ic_train_black_24dp, "#F94336"))
-        arrayList.add(DataModel("Item 6", R.drawable.ic_train_black_24dp, "#09A9FF"))
-        arrayList.add(DataModel("Item 7", R.drawable.ic_train_black_24dp, "#3E51B1"))
-        arrayList.add(DataModel("DashCam", R.drawable.ic_dash, "#0A9B88"))
-        arrayList.add(DataModel("Item 8", R.drawable.ic_train_black_24dp, "#09A9FF"))
-        arrayList.add(DataModel("Item 9", R.drawable.ic_train_black_24dp, "#3E51B1"))
-        arrayList.add(DataModel("Item 10", R.drawable.ic_train_black_24dp, "#673BB7"))
-        arrayList.add(DataModel("Item 11", R.drawable.ic_train_black_24dp, "#4BAA50"))
-        arrayList.add(DataModel("Item 12", R.drawable.ic_train_black_24dp, "#F94336"))
-        arrayList.add(DataModel("Item 13", R.drawable.ic_train_black_24dp, "#09A9FF"))
-        arrayList.add(DataModel("Item 14", R.drawable.ic_train_black_24dp, "#3E51B1"))
+        arrayList.add(DataModel(getString(R.string.train_ticket), R.drawable.ic_train_black_24dp, "#09A9FF"))
+        arrayList.add(DataModel(getString(R.string.taxi), R.drawable.ic_local_taxi_black_24dp, "#3E51B1"))
+        arrayList.add(DataModel(getString(R.string.concert_ticket), R.drawable.ic_music_note_black_24dp, "#673BB7"))
+        arrayList.add(DataModel(getString(R.string.shopping), R.drawable.ic_shopping_cart_black_24dp, "#4BAA50"))
+        arrayList.add(DataModel(getString(R.string.movie_ticket), R.drawable.ic_local_movies_black_24dp, "#F94336"))
+        arrayList.add(DataModel(getString(R.string.book), R.drawable.ic_book_black_24dp, "#09A9FF"))
+        arrayList.add(DataModel(getString(R.string.fitness), R.drawable.ic_directions_run_black_24dp, "#3E51B1"))
+        arrayList.add(DataModel(getString(R.string.bird_photo), R.drawable.ic_dash, "#0A9B88"))
+        arrayList.add(DataModel(getString(R.string.chat), R.drawable.ic_chat_black_24dp, "#09A9FF"))
+        arrayList.add(DataModel(getString(R.string.phone_call), R.drawable.ic_local_phone_black_24dp, "#3E51B1"))
+        arrayList.add(DataModel(getString(R.string.save_file), R.drawable.ic_cloud_black_24dp, "#673BB7"))
+        arrayList.add(DataModel(getString(R.string.book_flight), R.drawable.ic_flight_black_24dp, "#4BAA50"))
+        arrayList.add(DataModel(getString(R.string.map), R.drawable.ic_map_black_24dp, "#F94336"))
+        arrayList.add(DataModel(getString(R.string.auction), R.drawable.ic_gavel_black_24dp, "#09A9FF"))
+        arrayList.add(DataModel(getString(R.string.more_apps), R.drawable.ic_apps_black_24dp, "#3E51B1"))
 
         val adapter = RecyclerViewAdapter(this, arrayList, this)
         recyclerView.adapter = adapter
 
-
-        /**
-        AutoFitGridLayoutManager that auto fits the cells by the column width defined.
-         **/
-
+        // AutoFitGridLayoutManager that auto fits the cells by the column width defined.
         //val layoutManager = AutoFitGridLayoutManager(this, 500)
         //recyclerView.layoutManager = layoutManager
 
-        /**
-        Simple GridLayoutManager that spans two columns
-         **/
+
+        // Simple GridLayoutManager that spans two columns
         val manager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = manager
     }
 
     override fun onItemClick(item: DataModel) {
-        if (item.text == "DashCam") {
+        if (item.text == getString(R.string.bird_photo)) {
             startActivity(Intent(this, IncrementerActivity::class.java))
         } else {
-            Toast.makeText(applicationContext, item.text + " is clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, item.text, Toast.LENGTH_SHORT).show()
         }
     }
 }
